bf263af61a6f0ae4cdfa8e53ab5ed7b001ac333e
Pietra-Santos <pietra0608@gmail.com>
2021-09-06 21:12:09 -0300

    att config

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports={
-    dialect:'',
-    host:'',
-    username:'',
-    password:'',
-    database:'',
+    dialect:'postgres',
+    host:'chunee.db.elephantsql.com',
+    username:'gdelekue',
+    password:'6LXkGlwTZcHKlVliRqn6R98Ds_a7wr0h',
+    database:'gdelekue',
     define:{
         timeStamps: true,
         underscored: true,
