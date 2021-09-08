1714a019da7ee13cbe7b96a59911701d96b5efa7
Pietra-Santos <pietra0608@gmail.com>
2021-09-08 18:51:25 -0300

    user customer autentication

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@
 import { Router } from 'express';
 import EmployeeController from './app/controller/EmployeeController';
 import CustomerController from './app/controller/CustomerController';
+import SessionController from './app/controller/SessionController';
 
 const routes = new Router();
 
@@ -15,7 +16,8 @@ const routes = new Router();
  routes.get('/read_employees', EmployeeController.index);
 
  //Rotas Customer
- routes.post('/create_customers', CustomerController.store);
+ routes.post('/create_customer', CustomerController.store);
  routes.get('/read_customers', CustomerController.index);
+ routes.post('/create_session', SessionController.store);
 
  export default routes;
