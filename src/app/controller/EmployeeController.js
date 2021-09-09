97e9d227df3e5bc2788d9e96dac54a1ef21ae8b0
Pietra-Santos <pietra0608@gmail.com>
2021-09-08 21:33:56 -0300

    att EmployeeController

--- src/app/controller/EmployeeController.js
+++ src/app/controller/EmployeeController.js
@@ -7,21 +7,19 @@ class EmployeeController {
     };
 
     async index(req, res) {
-        const employee = await Employee.findAll();
-        return res.status(200).json(employee);
-    };
 
         //TODO Criação de GET
         const employee = await Employee.findOne({ where : {
                 email: req.body.email,
                 password_hash: await Employee.passwordHash(req.body.password)
-             }
+                }
         });
 
         return res.json(employee);
-        
+            
     };
 
+    
     async delete(req, res) {
         return res.status(200).json({ message: 'Delete realizado com sucesso.' });
     };
