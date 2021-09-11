2f15b2f7423a7dd3d0dc896b82db234dcf777c3f
Julia Minagawa <julia.minagawa@gmail.com>
2021-09-11 12:13:12 -0300

    atualização status

--- src/app/controller/DemandController.js
+++ src/app/controller/DemandController.js
@@ -1,49 +1,58 @@
-import Demand from '../models/Demand';
-
+import Demand from "../models/Demand";
+const { Op } = require("sequelize"); 
 
 class DemandController {
-    async store(req, res) {
+  async store(req, res) {
+    const { status_id } = req.body;
 
-        const { status_id } = req.body;
+    const demand = await Demand.create({
+      customer_id: req.userId,
+      status_id,
+    });
+    return res
+      .status(200)
+      .json({ message: "Pedido criado com sucesso.", demand });
+  }
 
-        const demand = await Demand.create({
-            customer_id: req.userId,
-            status_id,
-        });
-        return res.status(200).json({ message: 'Pedido criado com sucesso.', demand});
-    };
+  async index(req, res) {
+    const demand = await Demand.findAll({
+      where: {
+        status_id: { [Op.gt]: 1 },
+        customer_id: req.userId,
+      },
 
-    async index(req, res) {
-        const demand = await Demand.findAll({
-            attributes: ['id']
-        });
-        return res.status(200).json(id);
-    };
+      order: [['id', 'ASC']],
+      attributes: ['id', 'customer_id', 'status_id'],
+    });
+    return res.status(200).json(demand);
+  }
 
-    async delete(req, res) {
-        const demand = await Demand.findByPk(req.params.id);
-        if (!demand) {
-            return response(res, 404, 'Pedido não encontrado')
-        } else {
-            await demand.destroy();
-            return res.status(200).json({ message: 'Pedido removido com sucesso.' });
-        }
-    };
+  async delete(req, res) {
+    const demand = await Demand.findByPk(req.params.id);
+    if (!demand) {
+      return response(res, 404, "Pedido não encontrado");
+    } else {
+      await demand.destroy();
+      return res.status(200).json({ message: "Pedido removido com sucesso." });
+    }
+  }
 
-    async update(req, res) {
-        const demand = await Demand.findByPk(req.params.id);
-        if (!demand) {
-            return response(res, 404, 'Pedido não encontrado')
-        } else {
-        await demand.update(req.body);
-        return res.status(200).json({ message: 'Pedido atualizado com sucesso.' });
-        };
-    };
+  async update(req, res) {
+    const demand = await Demand.findByPk(req.params.id);
+    if (!demand) {
+      return response(res, 404, "Pedido não encontrado");
+    } else {
+      await demand.update(req.body);
+      return res
+        .status(200)
+        .json({ message: "Pedido atualizado com sucesso." });
+    }
+  }
 
-    async show(req, res) {
-        const demand = await Demand.findByPk(req.params.id);
-        return res.json({ id });
-    };
+  async show(req, res) {
+    const demand = await Demand.findByPk(req.params.id);
+    return res.json({ id });
+  }
 }
 
 export default new DemandController();
