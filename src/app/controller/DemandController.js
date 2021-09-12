f617540859b60894c8e8f4115dec50ece987750e
Evelyn Dias Santos <evelyn.informatica@gmail.com>
2021-09-12 15:39:41 -0300

    Inclusão de visualização de informações de tabelas many_to_many to/ Validação de token de Employess

--- src/app/controller/DemandController.js
+++ src/app/controller/DemandController.js
@@ -1,4 +1,8 @@
 import Demand from "../models/Demand";
+import Status from "../models/Status";
+import Customer from "../models/Customer";
+import OrderItens from '../models/OrderItens';
+import Product from "../models/Product";
 import {Op} from "sequelize"; 
 
 class DemandController {
@@ -68,8 +72,45 @@ class DemandController {
   }
 
   async show(req, res) {
-    const demand = await Demand.findByPk(req.params.id);
-    return res.json({ id });
+    const demand= await Demand.findByPk(req.params.id);
+
+    const {id,customer_id, status_id}= await Demand.findByPk(req.params.id);
+
+    const status = await Status.findOne({
+      where: {
+        id: status_id ,
+      },
+      attributes: ['status'],
+    });
+
+    const customer = await Customer.findOne({
+      where: {
+        id: customer_id ,
+      },
+      attributes: ['name'],
+    });
+
+    const orderItens = await OrderItens.findAll({
+      where: {
+        demand_id: id ,
+      },
+    });
+
+    var itens = [];
+
+    orderItens.forEach(
+      (orderItens) => {
+        itens.push((orderItens.dataValues.product_id));
+      }
+    )
+    const products = await Product.findAll({
+      where: {
+        id: itens 
+      },
+      attributes: ['description'],
+    });
+    
+    return res.json({id, customer_id, customer, status,demand,products});
   }
 }
 
