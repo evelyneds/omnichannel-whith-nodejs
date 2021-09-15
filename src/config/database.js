06d783c6e40fa032d84b9932291485ab7f9dfd41
Evelyn Dias Santos <evelyn.informatica@gmail.com>
2021-09-14 23:29:06 -0300

    Inclusão de variáveis de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 require('dotenv/config')
 
 module.exports={
-    dialect:'process.env.DB_DIALECT',
-    host:'process.env.DB_HOST',
-    username:'process.env.DB_USERNAME',
-    password:'process.envprocess.envDB_PASSWORD',
-    database:'process.env.DB_NAME',
+    dialect:process.env.DB_DIALECT,
+    host:process.env.DB_HOST,
+    username:process.env.DB_USERNAME,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
     define:{
         timeStamps: true,
         underscored: true,
